da6ab77b6a0e2162ee2fb12ca5e3e5472a2dd8fc
ElcimarSilva <elcimar8@gmail.com>
2022-02-03 18:14:56 -0300

    as

--- extrator_cep.py
+++ extrator_cep.py
@@ -1,7 +1,7 @@
 import re #Regular Expression -- RegEx
 
 endereco = "aklsmas malkdm ask maslkd masl  88032005"
-padrao = re.compile("[0-9][0-9][0-9][0-9][0-9][-]?[0-9][0-9][0-9]") # define o padão de cep
+padrao = re.compile("[0-9][0-9][0-9][0-9][0-9][-]?[0-9][0-9][0-9]") # define o padrão de cep
 busca = padrao.search(endereco) # retira no o cep do endereco
 if busca:
     cep = busca.group()
